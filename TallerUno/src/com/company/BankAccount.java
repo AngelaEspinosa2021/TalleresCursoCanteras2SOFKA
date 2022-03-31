46d855798e0a6823ce9dd99a55c5b883903b00d8
AngelaEspinosa2021 <angelaespinosa23871@ucaldas.edu.co>
2022-03-31 15:16:31 -0500

    se crean los atributos adicionales en la clase BanckAccount y se les hace su respectiva inicialización en los constructores.

--- TallerUno/src/com/company/BankAccount.java
+++ TallerUno/src/com/company/BankAccount.java
@@ -1,2 +1,36 @@
-package com.company;public class BankAccount {
+package com.company;
+
+public class BankAccount
+{
+    //variables nativas de la clase
+    public int accountNumber;
+    public String customerDocumentType;
+    public String customerDocumentNumber;
+    public String customerFullName;
+    private double saldo;
+    protected boolean activated;
+
+    //constructores
+    public BankAccount()
+    {
+        accountNumber=00000000001;
+        customerDocumentType="CC";
+        customerDocumentNumber="1053777489";
+        customerFullName="ANGELA MARIA ESPINOSA GRANADA";
+        saldo=0;
+        activated=false;
+    }
+
+    public BankAccount(int accountNumber, String customerDocumentType, String customerDocumentNumber, String customerFullName, double saldo,boolean activated)
+    {
+        this();
+        this.accountNumber=accountNumber;
+        this.customerDocumentType=customerDocumentType;
+        this.customerDocumentNumber=customerDocumentNumber;
+        this.customerFullName=customerFullName;
+        this.saldo=saldo;
+        this.activated=activated;
+    }
+    //fin constructores
+
 }
