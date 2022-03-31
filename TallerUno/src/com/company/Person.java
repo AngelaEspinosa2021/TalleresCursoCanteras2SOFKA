d3ea3c16799a5ebc2abaa593077c3bf179e347f0
AngelaEspinosa2021 <angelaespinosa23871@ucaldas.edu.co>
2022-03-31 14:14:55 -0500

    Se agregan los atributos adicionales a la clasePersona y se les hace su respectiva inicialización en los constructores.

--- TallerUno/src/com/company/Person.java
+++ TallerUno/src/com/company/Person.java
@@ -5,30 +5,39 @@ import java.util.Date;
 public class Person
 {
     //variables nativas de la clase
+    public String documentType;
+    public String documentNumber;
     public String name;
     public String lastName1;
     public String lastName2;
     public Date dateBirth;
     public float height;
+    public String phoneNumber;
 
     //constructores
     public Person()
     {
+        documentType="CC";
+        documentNumber="1053777489";
         name="Angela";
         lastName1="Espinosa";
         lastName2="Granada";
         //dateBirth=1987-06-17;
         height=163;
+        phoneNumber="3193452785";
     }
 
-    public Person(String name, String lastName1, String lastName2, Date dateBirth, float height)
+    public Person(String documentType, String documentNumber,  String name, String lastName1, String lastName2, Date dateBirth, float height, String phoneNumber)
     {
         this();
+        this.documentType = documentType;
+        this.documentNumber = documentNumber;
         this.name = name;
         this.lastName1 = lastName1;
         this.lastName2 = lastName2;
         this.dateBirth = dateBirth;
         this.height = height;
+        this.phoneNumber = phoneNumber;
     }
     //fin constructores
 
